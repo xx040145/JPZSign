87130cf9b6a030d07f2ac8dea6a0c00bddd4472a
zhangke <zhangke@ehousechina.com>
2017-12-22 15:42:54 +0800

    no message

--- JPZSign/ViewController.m
+++ JPZSign/ViewController.m
@@ -22,8 +22,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    self.toSignVCBtn = [[UIButton alloc] initWithFrame:CGRectMake(100, 200, 100, 60)];
-    [self.toSignVCBtn setTitle:@"去签名" forState:UIControlStateNormal];
+    self.toSignVCBtn = [[UIButton alloc] initWithFrame:CGRectMake(100, 200, 200, 44)];
+    [self.toSignVCBtn setTitle:@"goSignBtn" forState:UIControlStateNormal];
     self.toSignVCBtn.titleLabel.textColor = [UIColor whiteColor];
     self.toSignVCBtn.titleLabel.font = [UIFont systemFontOfSize:16.0];
     self.toSignVCBtn.backgroundColor = [UIColor redColor];
@@ -45,7 +45,9 @@
 - (void)signCompleteWithImage:(UIImage *)img base64str:(NSString *)base64str{
     self.signImgView.image = img;
     
-    //可以在这里执行网络请求、图片上传等操作，成功后pop掉签名页
+    //do something
+    
+    //pop signVC
     SignViewController *signVC = self.navigationController.viewControllers.lastObject;
     [signVC popViewController];
 }
