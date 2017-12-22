17bcf308012dcfdc708aaea8fadf358ac0834e13
zhangke <zhangke@ehousechina.com>
2017-12-22 15:47:43 +0800

    Revert "no message"
    This reverts commit 87130cf9b6a030d07f2ac8dea6a0c00bddd4472a.

--- JPZSign/ViewController.m
+++ JPZSign/ViewController.m
@@ -22,8 +22,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    self.toSignVCBtn = [[UIButton alloc] initWithFrame:CGRectMake(100, 200, 200, 44)];
-    [self.toSignVCBtn setTitle:@"goSignBtn" forState:UIControlStateNormal];
+    self.toSignVCBtn = [[UIButton alloc] initWithFrame:CGRectMake(100, 200, 100, 60)];
+    [self.toSignVCBtn setTitle:@"去签名" forState:UIControlStateNormal];
     self.toSignVCBtn.titleLabel.textColor = [UIColor whiteColor];
     self.toSignVCBtn.titleLabel.font = [UIFont systemFontOfSize:16.0];
     self.toSignVCBtn.backgroundColor = [UIColor redColor];
@@ -45,9 +45,7 @@
 - (void)signCompleteWithImage:(UIImage *)img base64str:(NSString *)base64str{
     self.signImgView.image = img;
     
-    //do something
-    
-    //pop signVC
+    //可以在这里执行网络请求、图片上传等操作，成功后pop掉签名页
     SignViewController *signVC = self.navigationController.viewControllers.lastObject;
     [signVC popViewController];
 }
